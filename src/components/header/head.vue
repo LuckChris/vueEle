11c797cba9257c860e1f5916b072883d381da699
wurong <wurong@shanhs.com>
2018-09-13 19:59:51 +0800

    add

--- src/components/header/head.vue
+++ src/components/header/head.vue
@@ -1,19 +1,34 @@
 <template lang='pug'>
-.header-wrapper
-  .header-content
-    .header-title.text-white.font-size-65(@click='reloadHandle') ele.me
-    .header-right
-      router-link(to='/login')
-        p.text-white.font-size-65 登录|注册
-
+#head_top
+    slot(name='logo')
+    slot(name='search')
+    .head-goback(v-if='goBack', @click="$router.go(-1)")
+        svg(width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" version="1.1")
+            polyline(points="12,18 4,9 12,0" style='fill:none;stroke:rgb(255,255,255);stroke-width:2')
+    router-link(:to="userInfo? '/profile' : '/login'" v-if='signinUP' class='head-login')
+        svg(class='user-avatar' v-if="userInfo")
+            use(xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#user")
+            span(class='login-span' v-else) 登录|注册
+    .title-head.ellipsis(v-if="headTitle")
+        span.title-text {{headTitle}}
+    slot(name='edit')
+    slot(name='msite-title')
+    slot(name='changecity')
+    slot(name='changeLogin')
 </template>
 <script>
+import {mapState} from 'vuex'
 export default{
   data () {
     return {
 
     }
   },
+  computed: {
+    ...mapState(['userInfo'])
+
+  },
+  props: ['signinUp', 'headTitle', 'gaBack'],
   methods: {
     reloadHandle () {
       window.location.reload()
